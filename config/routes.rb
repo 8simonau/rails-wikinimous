bca452de3c8d9f998b64a2e04c0edfc0da2cfc7a
Aurelien Simon <aureliensimon@hotmail.com>
2024-01-15 08:54:47 +0100

    -solved

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "articles#index", as: "articles"
+  get "articles/new", to: "articles#new"
+  get "articles/:id", to: "articles#show", as: "article"
+  post "/", to:"articles#create"
+  delete "articles/:id", to: "articles#delete"
+  get "articles/:id/edit", to: "articles#edit", as: "edit"
+  patch "articles/:id", to: "articles#update"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+(1..10).each do
+  article = Article.new(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph_by_chars(number: 3000))
+  article.save
+end
